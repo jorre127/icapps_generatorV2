22647c7e3094f63360ac63e43326d29a9a1f8449
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2021-12-03 15:20:36 +0100

    Updated viewmodel and screen creators

--- bin/icapps_generator.dart
+++ bin/icapps_generator.dart
@@ -9,13 +9,12 @@ import 'src/params.dart';
 var screenName = '';
 Params params;
 const NO_NAV_ARG = '--no-nav';
-const NO_KIWI_ARG = '--no-kiwi';
+const NO_GETIT_ARG = '--no-getIt';
 
 Future<void> main(List<String> args) async {
   final pubspecYaml = File(join(Directory.current.path, 'pubspec.yaml'));
   if (!pubspecYaml.existsSync()) {
-    throw Exception(
-        'This program should be run from the root of a flutter/dart project');
+    throw Exception('This program should be run from the root of a flutter/dart project');
   }
   if (args == null || args.isEmpty) {
     throw Exception('No arguments provided. 1 argument is required.');
@@ -36,25 +35,24 @@ Future<void> main(List<String> args) async {
   }
 
   final generateNav = arg2 != NO_NAV_ARG && arg3 != NO_NAV_ARG;
-  final generateKiwi = arg2 != NO_KIWI_ARG && arg3 != NO_KIWI_ARG;
+  final generateGetIt = arg2 != NO_GETIT_ARG && arg3 != NO_GETIT_ARG;
 
   await parsePubspec(pubspecYaml);
   print('Options:');
   print('Generate MainNavigator: $generateNav');
-  print('Generate Kiwi Dependency Tree: $generateKiwi');
+  print('Generate GetIt Dependency Tree: $generateGetIt');
   print('\n');
   print('\n');
   print('Generating a new screen called `$screenName`');
   createFolders();
   createFiles();
-  await FileCreatorHelper.updateInjector(params.projectName, screenName);
   if (generateNav) {
     await FileCreatorHelper.updateMainNavigator(params.projectName, screenName);
   }
   print('');
-  if (generateKiwi) {
-    print('Generate Kiwi tree...');
-    await FlutterHelper.regenerateKiwi();
+  if (generateGetIt) {
+    print('Generate GetIt tree...');
+    await FlutterHelper.regenerateGetIt();
   }
   print('Done!!!');
 }
@@ -81,17 +79,14 @@ void createFolders() {
 }
 
 void createFiles() {
-  final screenFile =
-      File(join('lib', 'screen', screenName, '${screenName}_screen.dart'));
-  final viewModelFile = File(
-      join('lib', 'viewmodel', screenName, '${screenName}_viewmodel.dart'));
+  final screenFile = File(join('lib', 'screen', screenName, '${screenName}_screen.dart'));
+  final viewModelFile = File(join('lib', 'viewmodel', screenName, '${screenName}_viewmodel.dart'));
 
   if (screenFile.existsSync()) {
     throw Exception('`lib/screen/${screenName}_screen.dart` already exists');
   }
   if (viewModelFile.existsSync()) {
-    throw Exception(
-        '`lib/viewmodel/${screenName}_viewmodel.dart` already exists');
+    throw Exception('`lib/viewmodel/${screenName}_viewmodel.dart` already exists');
   }
   print('Create `lib/screen/${screenName}_screen.dart`');
   screenFile.createSync(recursive: true);
--- bin/src/file_creator_helper.dart
+++ bin/src/file_creator_helper.dart
@@ -11,119 +11,76 @@ class FileCreatorHelper {
   static void createViewModelFile(String screenName) {
     final sb = StringBuffer()
       ..writeln("import 'package:flutter/material.dart';")
+      ..writeln("import 'package:icapps_architecture/icapps_architecture.dart';")
+      ..writeln("import 'package:injectable/injectable.dart';")
       ..writeln()
-      ..writeln(
-          'class ${CaseUtil.getCamelcase(screenName)}ViewModel with ChangeNotifier{')
+      ..writeln('@injectable')
+      ..writeln('class ${CaseUtil.getCamelcase(screenName)}ViewModel with ChangeNotifierEx{')
       ..writeln('  ${CaseUtil.getCamelcase(screenName)}Navigator _navigator;')
       ..writeln()
-      ..writeln(
-          '  Future<void> init(${CaseUtil.getCamelcase(screenName)}Navigator navigator) async {')
+      ..writeln('  Future<void> init(${CaseUtil.getCamelcase(screenName)}Navigator navigator) async {')
       ..writeln('    _navigator = navigator;')
       ..writeln('  }')
       ..writeln('}')
       ..writeln()
-      ..writeln(
-          'abstract class ${CaseUtil.getCamelcase(screenName)}Navigator {}');
+      ..writeln('abstract class ${CaseUtil.getCamelcase(screenName)}Navigator {}');
 
     // Write to file
-    File(join('lib', 'viewmodel', screenName, '${screenName}_viewmodel.dart'))
-        .writeAsStringSync(sb.toString());
+    File(join('lib', 'viewmodel', screenName, '${screenName}_viewmodel.dart')).writeAsStringSync(sb.toString());
   }
 
   static void createScreenFile(String projectName, String screenName) {
     final sb = StringBuffer()
-      ..writeln(
-          "import 'package:$projectName/viewmodel/$screenName/${screenName}_viewmodel.dart';")
-      ..writeln("import 'package:$projectName/di/kiwi_container.dart';")
+      ..writeln("import 'package:$projectName/viewmodel/$screenName/${screenName}_viewmodel.dart';")
+      ..writeln("import 'package:get_it/get_it.dart';")
       ..writeln("import 'package:flutter/material.dart';")
-      ..writeln("import 'package:provider/provider.dart';")
+      ..writeln("import 'package:$projectName/widget/provider/provider_widget.dart';")
       ..writeln()
-      ..writeln(
-          'class ${CaseUtil.getCamelcase(screenName)}Screen extends StatelessWidget implements ${CaseUtil.getCamelcase(screenName)}Navigator {')
+      ..writeln('class ${CaseUtil.getCamelcase(screenName)}Screen extends StatelessWidget implements ${CaseUtil.getCamelcase(screenName)}Navigator {')
       ..writeln("  static const String routeName = '$screenName';")
       ..writeln()
       ..writeln('  @override')
       ..writeln('  Widget build(BuildContext context) {')
-      ..writeln(
-          '    return ChangeNotifierProvider<${CaseUtil.getCamelcase(screenName)}ViewModel>(')
-      ..writeln(
-          '      child: Consumer<${CaseUtil.getCamelcase(screenName)}ViewModel>(')
-      ..writeln('        builder: (context, value, child) => Scaffold(')
+      ..writeln('    return ProviderWidget<${CaseUtil.getCamelcase(screenName)}ViewModel>(')
+      ..writeln('      create: () => GetIt.I.get()..init(this),')
+      ..writeln('      childBuilderWithViewModel: (context, viewModel, theme, localization) => Scaffold(')
       ..writeln('          body: Center(),')
       ..writeln('        ),')
       ..writeln('      ),')
-      ..writeln(
-          '      create: (context) => KiwiContainer.resolve()..init(this),')
       ..writeln('    );')
       ..writeln('  }')
       ..writeln('}');
 
     // Write to file
-    File(join('lib', 'screen', screenName, '${screenName}_screen.dart'))
-        .writeAsStringSync(sb.toString());
+    File(join('lib', 'screen', screenName, '${screenName}_screen.dart')).writeAsStringSync(sb.toString());
   }
 
-  static Future<void> updateInjector(
-      String projectName, String screenName) async {
-    final injectorFile = File(join('lib', 'di', 'injector.dart'));
-
-    final sb = StringBuffer()
-      ..writeln(
-          "import 'package:$projectName/viewmodel/$screenName/${screenName}_viewmodel.dart';");
-    await injectorFile
-        .openRead()
-        .transform(const Utf8Decoder())
-        .transform(const LineSplitter())
-        .forEach((l) {
-      if (l == '  void registerViewModelFactories();') {
-        sb.writeln(
-            '  @Register.factory(${CaseUtil.getCamelcase(screenName)}ViewModel)');
-      }
-      sb.writeln(l);
-    }).whenComplete(() {});
-    injectorFile.writeAsStringSync(sb.toString());
-  }
-
-  static Future<void> updateMainNavigator(
-      String projectName, String screenName) async {
-    final mainNavigatorFile =
-        File(join('lib', 'navigator', 'main_navigator.dart'));
+  static Future<void> updateMainNavigator(String projectName, String screenName) async {
+    final mainNavigatorFile = File(join('lib', 'navigator', 'main_navigator.dart'));
     if (!mainNavigatorFile.existsSync()) {
-      print(
-          '`lib/navigator/main_navigator.dart` does not exists. Can not add navigation logic.');
+      print('`lib/navigator/main_navigator.dart` does not exists. Can not add navigation logic.');
       return;
     }
 
     final sb = StringBuffer()
-      ..writeln(
-          "import 'package:$projectName/screen/$screenName/${screenName}_screen.dart';")
-      ..writeln(
-          "import 'package:$projectName/widget/general/flavor_banner.dart';");
+      ..writeln("import 'package:$projectName/screen/$screenName/${screenName}_screen.dart';")
+      ..writeln("import 'package:$projectName/widget/general/flavor_banner.dart';");
     var writeOnGenerateRoute = false;
-    await mainNavigatorFile
-        .openRead()
-        .transform(const Utf8Decoder())
-        .transform(const LineSplitter())
-        .forEach((l) {
+    await mainNavigatorFile.openRead().transform(const Utf8Decoder()).transform(const LineSplitter()).forEach((l) {
       if (l == '  Route onGenerateRoute(RouteSettings settings) {') {
         writeOnGenerateRoute = true;
       }
       if (l == '      default:' && writeOnGenerateRoute) {
         sb
-          ..writeln(
-              '      case ${CaseUtil.getCamelcase(screenName)}Screen.routeName:')
-          ..writeln(
-              '        return MaterialPageRoute(builder: (context) => FlavorBanner(child: ${CaseUtil.getCamelcase(screenName)}Screen()), settings: settings);');
+          ..writeln('      case ${CaseUtil.getCamelcase(screenName)}Screen.routeName:')
+          ..writeln('        return MaterialPageRoute(builder: (context) => FlavorBanner(child: ${CaseUtil.getCamelcase(screenName)}Screen()), settings: settings);');
       }
-      if (l ==
-          '  void closeDialog() => Navigator.of(context, rootNavigator: true).pop();') {
+      if (l == '  void closeDialog() => Navigator.of(context, rootNavigator: true).pop();') {
         sb
-          ..writeln(
-              '  void goTo${CaseUtil.getCamelcase(screenName)}() => navigationKey.currentState.pushNamed(${CaseUtil.getCamelcase(screenName)}Screen.routeName);')
+          ..writeln('  void goTo${CaseUtil.getCamelcase(screenName)}() => navigationKey.currentState.pushNamed(${CaseUtil.getCamelcase(screenName)}Screen.routeName);')
           ..writeln();
       }
-      if (l !=
-          "import 'package:$projectName/widgets/general/flavor_banner.dart';") {
+      if (l != "import 'package:$projectName/widgets/general/flavor_banner.dart';") {
         sb.writeln(l);
       }
     }).whenComplete(() {});
